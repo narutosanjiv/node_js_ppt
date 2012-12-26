8112a95617496b77b766378e92a467aa1cae35f0
sanjiv <narutosanjiv@gmail.com>
2012-12-27 02:17:57 +0530

    added stream file uploader

---
+++ node_examples/streaming/file_uploader.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var http = require('http');
+http.createServer(function(request, response) {
+  var newFile = fs.createWriteStream("readme_copy.md");
+  request.pipe(newFile);
+  request.on('end', function() {
+    response.end('uploaded!');
+  });
+}).listen(8080);
+
